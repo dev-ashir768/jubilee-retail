4546158590a88c9cec8cbff7fd743b15c8425d2c
dev-ashir768 <techashir167@gmail.com>
2025-09-16 14:33:19 +0500

    reove duplicate in add-plan

--- app/(dashboard)/products-plans/(plan)/add-plan/page.tsx
+++ app/(dashboard)/products-plans/(plan)/add-plan/page.tsx
@@ -1,15 +1,9 @@
+import { Metadata } from 'next'
 import AddPlan from '@/components/ui/plans/add-plan'
-import { Metadata } from 'next'
-import React from 'react'
-import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Add Plan | Jubilee Retail',
   
-}
-
-export const metadata: Metadata = {
-  title: 'Add Plan | Jubilee Retail',
 }
 
 const page = () => {
