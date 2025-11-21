57780cf506a250ab2fa71754eaddd7b57333cc5b
Ammar Ali <rajaammarali2003@gmail.com>
2025-11-21 15:03:09 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",   // ← bas yeh ek line kafi hai ab
-
   images: {
     remotePatterns: [
       {
-         protocol: "http",
+        protocol: "http",
         hostname: "162.211.84.202",
         port: "3003",
         pathname: "/uploads/**",
